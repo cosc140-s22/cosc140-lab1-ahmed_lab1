b8b837fcc38063d374c5ddccdb8dd657edec700b
Ahmed Kamran <akamran@colgate.edu>
2022-02-17 09:49:41 -0500

    prob1 complete

--- problem1.py
+++ problem1.py
@@ -1,6 +1,17 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Ahmed Kamran
+# Collaborator(s): none
 #
 
+'''
+    DESCRIPTION: 
+    Write a short program that asks for a number of seconds (as an integer), 
+    converts that value to a number of seconds, minutes, hours, and days, 
+    then prints the equivalent seconds, minutes, hours, and days. 
+'''
+numSec = int(input("How many seconds? "))
+minutes, seconds = divmod(numSec, 60)
+hours, minutes = divmod(minutes, 60)
+days, hours = divmod(hours, 24)
 
+print(F"{numSec} seconds is equivalent to {days} {'days' if (days>1) else 'day'}, {hours} {'hours' if (hours>1) else 'hour'}, {minutes} {'minutes' if (minutes>1) else 'minute'}, and {seconds} {'seconds' if (seconds>1) else 'second'}.")
--- problem2.py
+++ problem2.py
@@ -1,6 +1,18 @@
 #
-# Name:
-# Collaborator(s):
+# Name: Ahmed Kamran
+# Collaborator(s): none
 #
 
 
+'''
+Write a program that computes and prints the great circle distance on Earth 
+between any two different latitude/longitude coordinates, known as the Haversine distance.
+'''
+
+lat1, lon1 = (float(input("Give me a lat: ")),
+              float(input("Give me a lon: ")))
+
+lat2, lon2 = (float(input("Give me another lat: ")),
+              float(input("Give me another lon: ")))
+
+print(F"Coordinates: {(lat1,lon1)} => {(lat2,lon2)}")
